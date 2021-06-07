2defaa43f97a88cbd93d2fde25dde61d10736eb5
murchu27 <m.b.murphy15@gmail.com>
2021-06-07 21:56:43 +0100

    Removing config leftover when testing LEDs

--- keyboards/ferris/keymaps/murchu27/config.h
+++ keyboards/ferris/keymaps/murchu27/config.h
@@ -44,5 +44,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGBLIGHT_HUE_STEP 8
   #define RGBLIGHT_SAT_STEP 8
   #define RGBLIGHT_VAL_STEP 8
-  #define RGBLED_NUM 14
 #endif
